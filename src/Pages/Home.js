5e1c7af4b0022abf84d1ef79a47ba02d2b3c714e
Abdulwahab1-dev <wahab.proclamationhub@gmail.com>
2023-03-17 20:29:56 +0500

    responsive website

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Banner from '../Component/Main/Banner'
 import Header from '../Component/Main/Header'
-// import Newsletter from '../Component/Main/Newsletter'
-// import Prim from '../Component/Main/Prim'
+import Newsletter from '../Component/Main/Newsletter'
+import Prim from '../Component/Main/Prim'
 import Welcome from '../Component/Main/Welcome'
-// import Footer from '../Component/Main/Footer'
+import Footer from '../Component/Main/Footer'
 import Slider from '../Component/Main/Slider'
-// import Styledetails from '../Component/Main/Styledetails'
+import Styledetails from '../Component/Main/Styledetails'
 
 const Home = () => {
   return (
